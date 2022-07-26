53b05ac12b5b60b19584fb0b1dc29e4c401b8778
ProjectzJV <vitorg8_2011@hotmail.com>
2022-07-26 13:01:12 -0300

    Blog Pessoal utilizando React

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+    <div className='titulo'>
+      <h1>Oxóssi - Meu Orixá</h1> 
+      <h2>Minha personalidade</h2>
+    </div>
+
+      <img src="https://i.pinimg.com/564x/9e/f6/08/9ef6088f04e0f88a32818b378034f3f7.jpg" alt="Imagem Representativa" className="img"/>
+    </>
+
+  );
+}
+
+export default Home
